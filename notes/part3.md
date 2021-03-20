f8a44429a3dc61833cd1d4caec5bce2f25310397
Andrey Azov <andrey@ebi.ac.uk>
2021-03-20 17:36:18 +0000

    Fix action associated with a button

--- notes/part3.md
+++ notes/part3.md
@@ -1,2 +1,9 @@
 1) Change 1 – the project wouldn't start because of incorrect types in a single file. Typescript identified this file, so was trivial to fix.
 2) Change 2 - webpack started with npm start took a god-awful amount of time to rebuild assets. Turned out its mode, if not specified explicitly, is production. Changed it to development. The rebuild time went down from over a dozen of seconds to hundreds of milliseconds.
+3) Fixed an obvious mistake intended as a trap for the inattentive:
+
+```
+document.querySelector<HTMLDivElement>('.next').addEventListener('click', () => dispatch(actionPrev()));
+```
+
+a button with a `next` class was calling a `previous` action
--- part3/src/index.ts
+++ part3/src/index.ts
@@ -55,5 +55,5 @@ createThemeSelector(state$)
 document.querySelector<HTMLDivElement>('.set-light').addEventListener('click', () => dispatch(actionSetTheme('light')));
 document.querySelector<HTMLDivElement>('.set-dark').addEventListener('click', () => dispatch(actionSetTheme('dark')));
 document.querySelector<HTMLDivElement>('.prev').addEventListener('click', () => dispatch(actionPrev()));
-document.querySelector<HTMLDivElement>('.next').addEventListener('click', () => dispatch(actionPrev()));
+document.querySelector<HTMLDivElement>('.next').addEventListener('click', () => dispatch(actionNext()));
 document.querySelector<HTMLDivElement>('.restart').addEventListener('click', () => dispatch(actionRestart()));
